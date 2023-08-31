2f41e4171d05ba6493dfbb5493f3da6f5fd54b31
Kiragu-Maina <jumaliko711@gmail.com>
2023-08-31 17:06:16 +0300

    in asgi initialize asgi app before importing websocketurlpatterns

--- crashsite/asgi.py
+++ crashsite/asgi.py
@@ -6,7 +6,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from crash.routing import websocket_urlpatterns
+
 from channels.sessions import SessionMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crashsite.settings")
@@ -14,7 +14,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crashsite.settings")
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-import crash.routing
+from crash.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
