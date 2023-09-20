1b038f0a1aa4b8409036236d534d704f492cb99c
Kiragu-Maina <jumaliko711@gmail.com>
2023-09-20 03:02:20 +0300

    add pipeline

--- crash/management/commands/populate_users.py
+++ crash/management/commands/populate_users.py
@@ -1,30 +1,34 @@
 import os
 import json
+from django.core.management.base import BaseCommand
 import random
 from faker import Faker
 
 fake = Faker()
 from ...models import User
+class Command(BaseCommand):
+    help = 'Run the GameManager in the background'
 
-def generate_user_data(num_users):
-    users = []
-    for _ in range(num_users):
-        phone_number = fake.unique.phone_number()
-        user_name = fake.unique.user_name()
-        password = fake.password()
-        user_to_json = {
-            'phone_number': phone_number,
-            'user_name': user_name,
-            'password': password,
-        }
-        user = User.objects.create(phone_number=phone_number, user_name=user_name)
-        user.set_password(password)
-        user.save()
-        users.append(user_to_json)
+    def handle(self, *args, **kwargs):
+        
+        def generate_user_data(num_users):
+            users = []
+            for _ in range(num_users):
+                phone_number = fake.unique.phone_number()
+                user_name = fake.unique.user_name()
+                password = fake.password()
+                user_to_json = {
+                    'phone_number': phone_number,
+                    'user_name': user_name,
+                    'password': password,
+                }
+                user = User.objects.create(phone_number=phone_number, user_name=user_name)
+                user.set_password(password)
+                user.save()
+                users.append(user_to_json)
 
-    with open('users.json', 'w') as json_file:
-        json.dump(users, json_file)
+            with open('users.json', 'w') as json_file:
+                json.dump(users, json_file)
+        num_users = 300
+        generate_user_data(num_users)
 
-if __name__ == "__main__":
-    num_users = 200
-    generate_user_data(num_users)
---
+++ dockerfile
@@ -0,0 +1,27 @@
+# Use a base image with Python
+FROM python:3.9-slim
+
+# Set environment variables
+ENV PYTHONUNBUFFERED 1
+
+# Set the working directory
+WORKDIR /app
+
+# Copy the application code to the container
+COPY . /app/
+
+# Install system dependencies
+RUN apt-get update && apt-get install -y \
+    npm && \
+    npm install -g yuglify
+
+# Install Python dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Run database migrations and collect static files
+RUN python manage.py makemigrations && \
+    python manage.py migrate && \
+    python manage.py collectstatic --noinput
+
+# Start the application using Daphne and Celery
+CMD ["sh", "-c", "daphne -b 0.0.0.0 -p $PORT crashsite.asgi:application & python manage.py initadmin & python manage.py populate_users & celery -A crashsite beat & celery -A crashsite worker --loglevel=info"]
