50987a0244c77c55ab3542188848c4b201ec980a
Kiragu-Maina <jumaliko711@gmail.com>
2023-09-24 15:29:14 +0300

    tests

--- crash/tests.py
+++ crash/tests.py
@@ -1,97 +1,243 @@
-import json
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 import random
+import json
 import asyncio
+import logging
+import httpx
+import websockets
+import time
+import multiprocessing
 
-# Load user data from the JSON file
-with open('users.json', 'r') as json_file:
-    users = json.load(json_file)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
 
-# Function to simulate user interaction
-async def simulate_user(user):
-    print('simulate_user called')
-    phone_number = user['phone_number']
-    password = user['password']
+class WebSocketClient:
+    def __init__(self, host):
+        self.host = host
+        self.ws = None
 
-     # Set up ChromeOptions for headless mode
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')  # Run Chrome in headless mode
+    async def connect(self):
+        logging.info('Connecting to WebSocket...')
+        self.ws = await websockets.connect(self.host, compression=None, origin="https://django-crash-testing.up.railway.app")
 
-    # Initialize WebDriver for each user with headless mode
-    driver = webdriver.Chrome(options=chrome_options)
-    
-    
-    
-    try:
-        # Navigate to your website
-        driver.get('https://ee61-102-68-79-97.ngrok.io')
-        time.sleep(5)
+    async def send_message(self, message):
+        await self.ws.send(json.dumps(message))
+        
 
+    async def receive_message(self):
+        return await self.ws.recv()
 
-        # Navigate to the web page where the "Sign In" link is located
+    async def disconnect(self):
+        logging.info('Disconnecting WebSocket...')
+        await self.ws.close()
+
+class UserSimulator:
+    def __init__(self, user):
+        self.user = user
+        self.phone_number = user['phone_number']
+        self.password = user['password']
+        self.user_name = user['user_name']
+        self.host = "https://django-crash-testing.up.railway.app"
+        self.session = None
+        self.ws_client = None
+        self.ws_client2 = None
+        
+     
+        
+        self.wait_time = random.randint(5, 15)  # Add a random wait time between tasks
        
+        self.allow_betting = True
+        self.time_to_play = 10
+        self.crashed = False
+        self.cashout_event = asyncio.Event()
 
-        # Locate and click on the "Sign In" link
-        try:
-            sign_in_link = driver.find_element_by_link_text('Sign In')  # Using the link text to locate the element
-            sign_in_link.click()
-            print("Clicked on 'Sign In' link.")
-            try: 
-                driver.find_element_by_id('phone_number1').send_keys(phone_number)
-                driver.find_element_by_id('password').send_keys(password)
-                driver.find_element_by_id('login_submit_btn').click()
+        
+        
 
-                # Perform other interactions as needed
-                async with websockets.connect('wss:/ee61-102-68-79-97.ngrok.io/ws/realtime/') as websocket:
-                    
-                        
+    async def login(self):
+        logging.info(f"Logging in as {self.phone_number}...")
+        async with httpx.AsyncClient() as client:
+            response = await client.post(f'{self.host}/login/', data={
+                'username': self.phone_number,
+                'password': self.password,
+            }, timeout=30)
 
-                    # Receive a message
-                    response_json = await websocket.recv()
-                    response_data = json.loads(response_json)
-
-                    if response_data['type'] == "start_synchronizer":
-                        bet_amount = random.randint(1, 3000)
-                        driver.find_element_by_id('selected-amount').send_keys(str(bet_amount))  # Convert to string
-                        driver.find_element_by_id('bet-button').click()
-                        print("Clicked on 'BET'.")
-                                
-                
-                
-
-            except Exception as e:
-                print(f"Error for user {phone_number}: {str(e)}")
+            if response.status_code == 200:
+                logging.info("Login successful")
+                self.session = response.cookies
+                logging.info(response)
+                return True
+            else:               
+                logging.error("Login failed")
+                return False
             
-        except Exception as e:
-            print("Error:", str(e))
+
+    async def simulate_user(self, game_id):            
+
+        bet_amount = random.randint(1, 3000)
+        bet_amount_str = str(bet_amount)
+        balloon_chosen = random.randint(1, 4)
+        selected_amount_input = bet_amount_str
+        group_name = f"group_{balloon_chosen}"
+        
+
+        async def place_bet():
+            start_time = time.time()  # Get the current time
+    
+            while time.time() - start_time <= 15:  # Retry within a 10-second window
+                time_to_wait = round(random.uniform(0.1, 2), 2)
+                await asyncio.sleep(time_to_wait)
+                async with httpx.AsyncClient(cookies=self.session) as client:
+                    response = await client.post(
+                        f'{self.host}/placebet/',
+                        data={'bet_amount': selected_amount_input, 'group_name': group_name},
+                        timeout=30
+                    )
+
+                    if response.status_code == 200:
+                        logging.info("Bet placement successful")
+                        logging.info("Clicked on 'BET'")
+                        
+                        break  # Break out of the retry loop on success
+                    else:
+                        time_to_wait = round(random.uniform(0.1, 2), 2)
+                        await asyncio.sleep(time_to_wait)
+                        logging.warning(f"Bet placement failed. Retrying in {time_to_wait} second(s)...")
+                        
+            else:
+                logging.warning("Exceeded the 5-second time window. Aborting bet placement.")
+                return
+            
+
+        await place_bet()
+        
+        await self.bet_placing_and_cashout(group_name, game_id)
+        
+
+        
+
+    async def bet_placing_and_cashout(self, group_name, game_id):
+        self.ws_client2 = WebSocketClient(f'wss://django-crash-testing.up.railway.app/ws/real_time_updates/{group_name}/')
+       
+        await self.ws_client2.connect()
+
+        try:
+            while True:
+                self.crashed = False
+                message = await self.ws_client2.receive_message()
+                logging.info(message)
+                message_data = json.loads(message)
+                message_type = message_data.get('type')
+
+                if message_type == "crash_instruction":
+                    self.crashed = True
+                    self.cashout_event.set()
+                    logging.info("Heard 'crash_instruction'")
+                    await self.ws_client2.disconnect()
+                    return
+                    
+                    
+                    
+                elif message_type == "count_update":
+                    
+                    await self.cashout(group_name, game_id)
+                    
+                    return
+                    
+                
+                    
+        finally:
+            await self.ws_client2.disconnect()
+           
+    async def cashout(self, group_name, game_id):
+            time_to_wait = round(random.uniform(1, 20), 2)
+            self.time_to_play = time_to_wait
+            update_interval = 0.01
+            delay = 0.05
+            count = 1
+            logging.info(f"Waiting for {time_to_wait} seconds...")
+
+            try:
+                while count <= time_to_wait:
+                    # Check for cancellation request
+                    if self.cashout_event.is_set():
+                        logging.info("Cashout operation canceled.")
+                        return
+
+                    await asyncio.sleep(delay)
+                    if not self.crashed:
+                        count += update_interval
+                    else:
+                        logging.info('Game crashed')
+                        break
+
+                # Signal that the cashout operation is ready to proceed
+                self.cashout_event.set()
+                await asyncio.sleep(5)
+                logging.info("Sending 'cashout_validate' message")
+                message = {'type': 'cashout_validate', 'multiplier': time_to_wait, 'game_id': game_id, 'user_name': self.user_name, 'password': 'qwertyadu'}
+                await self.ws_client2.send_message(message)
+            except asyncio.CancelledError:
+                logging.info("Cashout operation canceled.")
+                raise  # Re-raise the CancelledError if needed
 
 
+        
+    
+async def login_users(selected_users):
+    user_simulators = [UserSimulator(user) for user in selected_users]
+    logins = [user_simulator.login() for user_simulator in user_simulators]
+    await asyncio.gather(*logins)  # Gather and await both user_simulators and logins concurrently
+    return user_simulators
 
-        driver.find_element_by_id('phone_number_input').send_keys(phone_number)
-        driver.find_element_by_id('password_input').send_keys(password)
-        driver.find_element_by_id('login_button').click()
+async def run_simulations(user_simulators, game_id):
+    tasks = [user_simulator.simulate_user(game_id) for user_simulator in user_simulators]
+    await asyncio.gather(*tasks)
+    
 
-        # Perform other interactions as needed
+# Function to simulate user
+def simulate_user(user_simulator, game_id):
+    asyncio.run(user_simulator.simulate_user(game_id))
 
+async def main():
+    with open('users-prod.json', 'r') as json_file:
+        users = json.load(json_file)
+
+    num_users = 5
+    selected_users = random.sample(users, num_users)
+
+    # Create and login users concurrently
+    user_simulators = await login_users(selected_users)
+
+    # Create a WebSocket client
+    ws_client = WebSocketClient('wss://django-crash-testing.up.railway.app/ws/realtime/')
+    await ws_client.connect()
+
+    while True:
+        message = await ws_client.receive_message()
+        logging.info(message)
+        message_data = json.loads(message)
+        message_type = message_data.get('type')
+        
+        active_processes = []
+
+        if message_type == "start_synchronizer":
+            # Terminate any active processes
+            for process in active_processes:
+                if process.is_alive():
+                    process.terminate()
+                    process.join()
+            active_processes.clear()
+
+            # Start new processes
+            for user_simulator in user_simulators:
+                process = multiprocessing.Process(target=simulate_user, args=(user_simulator, message_data.get('game_id')))
+                active_processes.append(process)
+                process.start()
+
+        else:
+            pass
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
     except Exception as e:
-        print(f"Error for user {phone_number}: {str(e)}")
-    finally:
-        # Close the browser when done
-        driver.quit()
-
-num_users = 5
-print('starting loop')
-# Create an event loop for asyncio
-loop = asyncio.get_event_loop()
-
-# Create a list of tasks to run concurrently
-tasks = [simulate_user(user) for user in users]
-
-# Run the tasks concurrently
-loop.run_until_complete(asyncio.gather(*tasks))
-
-# All sessions are now complete
-print("All sessions are complete.")
+        logging.info(str(e))
