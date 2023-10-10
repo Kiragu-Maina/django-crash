caaa76c6e60f047ed637a8ccb0cc7c67382338fa
Kiragu-Maina <jumaliko711@gmail.com>
2023-10-11 02:44:49 +0300

    update newphaser, add production_settings

--- crashsite/settings.py
+++ crashsite/settings.py
@@ -1,6 +1,6 @@
-import os
+# import os
 
-if os.environ.get("DJANGO_ENV") == "PRODUCTION":
-    from .production_settings import *
-else:
-    from .local_settings import *
+# if os.environ.get("DJANGO_ENV") == "PRODUCTION":
+from .production_settings import *
+# else:
+#     from .local_settings import *
