dad6ec3884a52ab5ef3eafe08c1e750f0f267f2c
Kiragu-Maina <jumaliko711@gmail.com>
2023-10-20 14:01:04 +0300

    bet oon last game

--- crashsite/settings.py
+++ crashsite/settings.py
@@ -2,5 +2,5 @@ import os
 
 if os.environ.get("DJANGO_ENV") == "PRODUCTION":
     from .production_settings import *
-else:
-    from .local_settings import *
+# else:
+#     from .local_settings import *
